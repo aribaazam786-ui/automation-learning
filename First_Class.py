987bbced64fe9dcdc27c8ee8a3517c206106cc48
aribaazam786-ui <aribaazam786@gmail.com>
2026-01-18 00:46:57 +0500

    Add files via upload

---
+++ First_Class.py
@@ -0,0 +1,26 @@
+#creating class
+class car:
+
+    def __init__(self):
+        self
+
+#method
+    def start(self):
+        print("Car is starting")
+
+    def stop(self):
+        print("Car is stop")
+
+    def brand(self):
+        print("Toyota")
+    def color(self):
+        print("Red")
+
+#creating an object
+my_car = car()
+
+#Accessing
+my_car.start()
+my_car.stop()
+my_car.brand()
+my_car.color()
