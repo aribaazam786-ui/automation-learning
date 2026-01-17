c353ef3fafd4ef4532003d2c1acae1be4223348c
aribaazam786-ui <aribaazam786@gmail.com>
2026-01-18 00:23:57 +0500

    Add files via upload

---
+++ for_loop.py
@@ -0,0 +1,19 @@
+"""for i in range(1,6):
+    print(i)
+fruits = ['apple','banana','mango','kiwi','pineapple']
+for x in fruits:
+    print(x)
+    if(x == 'kiwi'):
+        break
+print("Executed")"""
+
+#Nested Loops
+adj=['red','big','tasty']
+fruit = ['apple','banana','mango','kiwi','pineapple']
+for y in adj:
+    for z in fruit:
+        print(y,z)
+
+#for loops is used for sequence
+
+
